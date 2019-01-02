d9b2915105ec262a10ee7a8b32d47a9563705d3e
reowen <russ.e.owen@gmail.com>
2019-01-02 12:44:08 -0500

    Use default light settings in test

--- park_assist.py
+++ park_assist.py
@@ -31,82 +31,82 @@ class StopLight:
         GPIO.setup(red_pin, GPIO.OUT) # Set red LED pin as an output
         GPIO.setup(yellow_pin, GPIO.OUT) # Set yellow LED pin as an output
         GPIO.setup(green_pin, GPIO.OUT) # Set green LED pin as an output
-    
-    def toggle_lights(self, red=False, yellow=False, green=False, all=False): 
+
+    def toggle_lights(self, red=False, yellow=False, green=False, all=False):
         """
-        Controls the stoplights. If the color arguments are set to True, it will turn on the corresponding lights. All colors are 
-        set to False by default, so not explicitly setting a color to True will turn it off (i.e., this function with no arguments 
-        will turn off all the lights. 
-        
-        Args: 
-            red (bool): If =True, the function will turn on the red light. If =False, it will turn it off. By default, this argument =False. 
-            yellow (bool): If =True, the function will turn on the yellow light. If =False, it will turn it off. By default, this argument =False. 
-            green (bool): If =True, the function will turn on the green light. If =False, it will turn it off. By default, this argument =False. 
+        Controls the stoplights. If the color arguments are set to True, it will turn on the corresponding lights. All colors are
+        set to False by default, so not explicitly setting a color to True will turn it off (i.e., this function with no arguments
+        will turn off all the lights.
+
+        Args:
+            red (bool): If =True, the function will turn on the red light. If =False, it will turn it off. By default, this argument =False.
+            yellow (bool): If =True, the function will turn on the yellow light. If =False, it will turn it off. By default, this argument =False.
+            green (bool): If =True, the function will turn on the green light. If =False, it will turn it off. By default, this argument =False.
             all (bool): If =True, the function will turn on all the lights. If =False, it will only turn on the colors specified as true.
-        """ 
-        # Validate arguments 
-        assert isinstance(red, bool), "'red' must be a boolean (True or False)." 
-        assert isinstance(yellow, bool), "'yellow' must be a boolean (True or False)." 
-        assert isinstance(green, bool), "'green' must be a boolean (True or False)." 
-        assert isinstance(all, bool), "'all' must be a boolean (True or False)." 
-        
-        # Toggle the lights 
-        if all: 
+        """
+        # Validate arguments
+        assert isinstance(red, bool), "'red' must be a boolean (True or False)."
+        assert isinstance(yellow, bool), "'yellow' must be a boolean (True or False)."
+        assert isinstance(green, bool), "'green' must be a boolean (True or False)."
+        assert isinstance(all, bool), "'all' must be a boolean (True or False)."
+
+        # Toggle the lights
+        if all:
             GPIO.output(self.red_pin, True)
             GPIO.output(self.yellow_pin, True)
-            GPIO.output(self.green_pin, True)  
-        else: 
+            GPIO.output(self.green_pin, True)
+        else:
             GPIO.output(self.red_pin, red)
             GPIO.output(self.yellow_pin, yellow)
-            GPIO.output(self.green_pin, green) 
-        
-    def blink(self, red=False, yellow=False, green=False, all=False): 
+            GPIO.output(self.green_pin, green)
+
+    def blink(self, red=False, yellow=False, green=False, all=False):
         """
-        Function that blinks the lights. Takes the corresponding color, and blinks the colors that are set to True. 
+        Function that blinks the lights. Takes the corresponding color, and blinks the colors that are set to True.
         """
-        self.toggle_lights() # Turn off all lights. 
-        self.toggle_lights(red=red, yellow=yellow, green=green, all=all) # Turn on specified lights. 
-        time.sleep(0.5) # Pause for half a second.  
-        self.toggle_lights() # Turn off all lights. 
-        
-    def blink_multi(self, blinks=5, red=False, yellow=False, green=False, all=False): 
+        self.toggle_lights() # Turn off all lights.
+        self.toggle_lights(red=red, yellow=yellow, green=green, all=all) # Turn on specified lights.
+        time.sleep(0.5) # Pause for half a second.
+        self.toggle_lights() # Turn off all lights.
+
+    def blink_multi(self, blinks=5, red=False, yellow=False, green=False, all=False):
         """
-        Function that blinks the lights a specified number of times. By default, the function blinks 5 times. 
-        
-        Args: 
-            blinks (int): An integer with the number of times the light will blink. By default, the light blinks 5 times. 
-            red (bool): If =True, the function will blink the red light. If =False, it will remain off. By default, this argument =False. 
-            yellow (bool): If =True, the function will blink the yellow light. If =False, it will remain off. By default, this argument =False. 
-            green (bool): If =True, the function will blink the green light. If =False, it will remain off. By default, this argument =False. 
-            all (bool): If =True, the function will blink all the lights. If =False, it will only blink the specified colors. By default, this argument =False. 
+        Function that blinks the lights a specified number of times. By default, the function blinks 5 times.
+
+        Args:
+            blinks (int): An integer with the number of times the light will blink. By default, the light blinks 5 times.
+            red (bool): If =True, the function will blink the red light. If =False, it will remain off. By default, this argument =False.
+            yellow (bool): If =True, the function will blink the yellow light. If =False, it will remain off. By default, this argument =False.
+            green (bool): If =True, the function will blink the green light. If =False, it will remain off. By default, this argument =False.
+            all (bool): If =True, the function will blink all the lights. If =False, it will only blink the specified colors. By default, this argument =False.
         """
-        # Validate the blinks argument 
-        assert isinstance(blinks, int), "'blinks' must be an integer." 
-        
-        # Blink the specified number of times 
-        for i in range(0, blinks): 
-            self.blink(red=red, yellow=yellow, green=green, all=all) 
-            
-    def test_lights(self): 
+        # Validate the blinks argument
+        assert isinstance(blinks, int), "'blinks' must be an integer."
+
+        # Blink the specified number of times
+        for i in range(0, blinks):
+            self.blink(red=red, yellow=yellow, green=green, all=all)
+
+    def test_lights(self):
         """
-        Runs a test to ensure the lights are working correctly. 
+        Runs a test to ensure the lights are working correctly.
         """
-        print("All lights on...") 
-        self.toggle_lights(all=True) 
-        time.sleep(1) 
-        
-        print("Red light on...") 
-        self.toggle_lights(red=True) 
-        time.sleep(1) 
-        
-        print("Yellow light on...") 
-        self.toggle_lights(yellow=True) 
-        time.sleep(1) 
-        
-        print("Green light on...") 
-        self.toggle_lights(green=True) 
-        time.sleep(1) 
-        
+        print("All lights on...")
+        self.toggle_lights(all=True)
+        time.sleep(1)
+
+        print("Red light on...")
+        self.toggle_lights(red=True)
+        time.sleep(1)
+
+        print("Yellow light on...")
+        self.toggle_lights(yellow=True)
+        time.sleep(1)
+
+        print("Green light on...")
+        self.toggle_lights(green=True)
+        time.sleep(1)
+
         print("Blink red.")
         self.blink(red=True)
 
@@ -117,8 +117,8 @@ class StopLight:
         self.blink(green=True)
 
         print("Blink all.")
-        self.blink(all=True) 
-        
+        self.blink(all=True)
+
         print("Blink red 3 times.")
         self.blink_multi(blinks=3, red=True)
 
@@ -129,14 +129,14 @@ class StopLight:
         self.blink_multi(blinks=3, green=True)
 
         print("Blink all 3 times.")
-        self.blink_multi(blinks=3, all=True) 
-        
-     
+        self.blink_multi(blinks=3, all=True)
+
+
 
 
 if __name__ == "__main__":
     # Test the red, green and yellow are working correctly:
-    lgt = StopLight(red_pin=19, yellow_pin=13, green_pin=26)
-    lgt.test_lights() 
+    lgt = StopLight()
+    lgt.test_lights()
 
     GPIO.cleanup()
