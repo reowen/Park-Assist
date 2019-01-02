e82712d69d02e3af9b9da199e5c35cd4631831dc
reowen <russ.e.owen@gmail.com>
2019-01-02 13:58:43 -0500

    Debug activation of PIR sensor

--- park_assist.py
+++ park_assist.py
@@ -143,6 +143,8 @@ class MotionSensor():
         # Set GPIO pin as an input
         GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
         GPIO.setup(pin, GPIO.IN)
+        time.sleep(2)
+        print("PIR Motion Sensor activated.")
 
     def detect_motion(self):
         """ Returns True if motion is detected, False otherwise. """
